e99f6edd0362b7b018523db8c9ae65873ec9e7ff
LJY <2169602167@qq.com>
2024-10-08 11:32:44 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
+name = 'Grey Li'
+movies = [
+{'title': 'JDFNFIWBWUSD', 'year':'1988'},
+{'title': 'JFHFTHDFNSF', 'year':'1975'},
+{'title': 'JFGFB DFGEV FSGV', 'year':'1998'},
+{'title': 'ZFGGSR FGDF', 'year':'2000'},
+{'title': 'sdgfdbc dfbgd', 'year':'1234'},
+{'title': 'svdsvs d', 'year':'1345'},
+{'title': 'aaa', 'year':'1567'},
+{'title': 'cccc', 'year':'1678'},
+]
+
 @app.route('/')
-def hello():
-    return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
+def index():
+    return render_template('index.html', name=name, movies=movies)
